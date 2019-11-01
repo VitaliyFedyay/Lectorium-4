b4f9bc73e39752b32c0fab361c40dbeaea9f2263
Федяй Виталий <vfedyay@hold.bayadera.ua>
2019-11-01 11:39:32 +0200

    added mySort

--- task 3/main.js
+++ task 3/main.js
@@ -1,14 +1,14 @@
-/*Array.prototype.myPush = function(arr) {
+Array.prototype.myPush = function () {
   for (let index = 0; index < arguments.length; index++) {
-    this[this.length] += arguments[index]
+    this[this.length] = arguments[index]
   }
-}*/
+}
 
 Array.prototype.myFilter = function (callback) {
   let newArr = []
   for (let index = 0; index < this.length; index++) {
     if (callback(this[index], index, this)) {
-      newArr.push(this[index])
+      newArr.myPush(this[index])
     }
   }
   return newArr
@@ -19,13 +19,14 @@ function matrix(R, C, r0, c0) {
   let start = [[r0, c0]]
   for (let i = 1; i <= n; i++) {
 
-    for (let j = 1 - i; j <= i; j++) start.push([r0 + j, c0 + i])
-    for (let j = i - 1; j >= -i; j--) start.push([r0 + i, c0 + j])
-    for (let j = i - 1; j >= -i; j--) start.push([r0 + j, c0 - i])
-    for (let j = 1 - i; j <= i; j++) start.push([r0 - i, c0 + j])
+    for (let j = 1 - i; j <= i; j++) start.myPush([r0 + j, c0 + i])
+    for (let j = i - 1; j >= -i; j--) start.myPush([r0 + i, c0 + j])
+    for (let j = i - 1; j >= -i; j--) start.myPush([r0 + j, c0 - i])
+    for (let j = 1 - i; j <= i; j++) start.myPush([r0 - i, c0 + j])
 
   }
-  let result = start.myFilter(function(x) {
+
+  let result = start.myFilter(function (x) {
     return x[0] >= 0 && x[0] < R && x[1] >= 0 && x[1] < C
   })
   console.table(result)
